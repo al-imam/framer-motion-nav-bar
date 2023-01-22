9428fafc44cd33fdf6901991a91eb6ba30890f03
al-imam <alimam01828@gmail.com>
2023-01-22 22:04:48 +0600

    create working dropdown menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import classes from "./app.module.css";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRef, useState } from "react";
 
 function App() {
   return (
@@ -9,20 +11,39 @@ function App() {
 }
 
 function Ul() {
+  const [open, setOpen] = useState(false);
+
   return (
     <ul className={classes.navbarNav}>
       <li className={classes.navItem}>Home</li>
 
-      <li className={classes.navItem}>
+      <motion.li
+        onMouseEnter={() => setOpen(true)}
+        onMouseLeave={() => setOpen(false)}
+        className={classes.navItem}
+      >
         <button className={classes.dropdownToggler}>Department</button>
-        <ul className={classes.dropdownMenu}>
-          <li>Computer Technology</li>
-          <li>Electrical Technology</li>
-          <li>Civil Technology</li>
-          <li>Electronics Technology</li>
-          <li>Architecture Technology</li>
-        </ul>
-      </li>
+        <AnimatePresence>
+          {open && (
+            <motion.ul
+              initial={{ scale: 0.8, opacity: 0, y: -10 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.8, opacity: 0 }}
+              transition={{
+                duration: 0.15,
+                ease: "easeOut",
+              }}
+              className={classes.dropdownMenu}
+            >
+              <li onClick={() => setOpen(false)}>Computer Technology</li>
+              <li>Electrical Technology</li>
+              <li>Civil Technology</li>
+              <li>Electronics Technology</li>
+              <li>Architecture Technology</li>
+            </motion.ul>
+          )}
+        </AnimatePresence>
+      </motion.li>
 
       <li className={classes.navItem}>Contact Us</li>
       <li className={classes.navItem}>About Us</li>
