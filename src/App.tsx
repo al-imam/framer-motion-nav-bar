17ac4fededc7ed813dcf33d98ed1fcf40f9ba4a9
al-imam <alimam01828@gmail.com>
2023-01-22 22:08:47 +0600

    make dropdown reuseable

--- src/App.tsx
+++ src/App.tsx
@@ -17,33 +17,7 @@ function Ul() {
     <ul className={classes.navbarNav}>
       <li className={classes.navItem}>Home</li>
 
-      <motion.li
-        onMouseEnter={() => setOpen(true)}
-        onMouseLeave={() => setOpen(false)}
-        className={classes.navItem}
-      >
-        <button className={classes.dropdownToggler}>Department</button>
-        <AnimatePresence>
-          {open && (
-            <motion.ul
-              initial={{ scale: 0.8, opacity: 0, y: -10 }}
-              animate={{ scale: 1, opacity: 1 }}
-              exit={{ scale: 0.8, opacity: 0 }}
-              transition={{
-                duration: 0.15,
-                ease: "easeOut",
-              }}
-              className={classes.dropdownMenu}
-            >
-              <li onClick={() => setOpen(false)}>Computer Technology</li>
-              <li>Electrical Technology</li>
-              <li>Civil Technology</li>
-              <li>Electronics Technology</li>
-              <li>Architecture Technology</li>
-            </motion.ul>
-          )}
-        </AnimatePresence>
-      </motion.li>
+      <DropDown setOpen={setOpen} open={open} />
 
       <li className={classes.navItem}>Contact Us</li>
       <li className={classes.navItem}>About Us</li>
@@ -52,3 +26,51 @@ function Ul() {
 }
 
 export default App;
+
+function DropDown({
+  setOpen,
+  open,
+}: {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  open: boolean;
+}) {
+  return (
+    <motion.li
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      className={classes.navItem}
+    >
+      <button className={classes.dropdownToggler}>Department</button>
+      <AnimatePresence>
+        {open && (
+          <motion.ul
+            initial={{
+              scale: 0.8,
+              opacity: 0,
+              y: -10,
+            }}
+            animate={{
+              scale: 1,
+              opacity: 1,
+            }}
+            exit={{
+              scale: 0.8,
+              opacity: 0,
+            }}
+            transition={{
+              duration: 0.15,
+              ease: "easeOut",
+            }}
+            className={classes.dropdownMenu}
+          >
+            <li onClick={() => setOpen(false)}>Computer Technology</li>
+            <li>Electrical Technology</li>
+            <li>Civil Technology</li>
+            <li>Electronics Technology</li>
+            <li>Architecture Technology</li>
+          </motion.ul>
+        )}
+      </AnimatePresence>
+    </motion.li>
+  );
+}
